a4cc6ee962d14f96c48c11588a59a97d3464ba8a
aurora_desiree <aurora_desiree@outlook.com>
2018-09-24 15:29:21 -0300

    ASP V1

---
+++ ASPNETMVC/CRUDASPNetMVC/CRUDASPNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDASPNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //codigo da regra de negocio
+            return View();
+        }
+    }
+}
---
+++ ASPNETMVC/CRUDASPNetMVC/CRUDASPNetMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUDASPNetMVC.MvcApplication" Language="C#" %>
