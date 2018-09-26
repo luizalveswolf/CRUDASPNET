583d448f962afad727533a41468d9b820426f3be
aurora_desiree <aurora_desiree@outlook.com>
2018-09-26 04:53:55 -0300

    Connectrins funcionando
    corrigido a connectionString

---
+++ ASPNETMVC/CRUDASPNetMVC/CursoEntityFrameWorkAlura/EntidadesContext.cs
@@ -0,0 +1,15 @@
+﻿using CursoEntityFrameWorkAlura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoEntityFrameWorkAlura
+{
+    public class EntidadesContext:DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; } 
+    }
+}
---
+++ ASPNETMVC/CRUDASPNetMVC/CursoEntityFrameWorkAlura/Program.cs
@@ -0,0 +1,26 @@
+﻿using CursoEntityFrameWorkAlura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoEntityFrameWorkAlura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntidadesContext contexto = new EntidadesContext();
+            contexto.Database.CreateIfNotExists();
+
+            Usuario usuario = new Usuario() { Nome = "Luiz", Senha = "123" };
+            contexto.Usuarios.Add(usuario);
+
+            contexto.SaveChanges();
+
+            contexto.Dispose();
+
+        }
+    }
+}
