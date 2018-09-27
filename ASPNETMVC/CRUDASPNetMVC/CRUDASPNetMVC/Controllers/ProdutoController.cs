3581a9c533bb8c23793b6f9c37cffaffa4a90c8a
aurora_desiree <aurora_desiree@outlook.com>
2018-09-27 05:36:01 -0300

    Metodo Adiciona criado

--- ASPNETMVC/CRUDASPNetMVC/CRUDASPNetMVC/Controllers/ProdutoController.cs
+++ ASPNETMVC/CRUDASPNetMVC/CRUDASPNetMVC/Controllers/ProdutoController.cs
@@ -10,7 +10,7 @@ namespace CRUDASPNetMVC.Controllers
 {
     public class ProdutoController : Controller
     {
-        //logica d negocio
+        //logica de negocio
 
         // GET: Produto
         public ActionResult Index()
@@ -19,6 +19,45 @@ namespace CRUDASPNetMVC.Controllers
             IList<Produto> produtos = dao.Lista();
             ViewBag.Produtos = produtos;
             return View();
+        }
+
+        //Criação do cadastro de produto usando o formulario
+        public ActionResult Form()
+        {
+            CategoriaDAO categoriasDAO = new CategoriaDAO();
+            IList<CategoriaDoProduto> categorias = categoriasDAO.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        //Modelo 1 passando os valores por parametros
+        //Metodo para adicionar o produto ao carrinho
+        //Recebe o que foi digitado no formulario e salva no objeto Produto atraves do ProdutoDAO
+        //public ActionResult Adiciona(String nome, float preco, String descricao, int quantidade, int categoriaId)
+        //{
+        //    Produto produto = new Produto()
+        //    {
+        //        Nome = nome,
+        //        Preco = preco,
+        //        Descricao = descricao,
+        //        Quantidade = quantidade,
+        //        CategoriaId = categoriaId
+        //    };
+
+        //    ProdutoDAO dao = new ProdutoDAO();
+        //    dao.Adiciona(produto);
+
+        //    return View();
+        //}
+
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            ProdutoDAO dao = new ProdutoDAO();
+            dao.Adiciona(produto);
+
+            return RedirectToAction("Index","Produto");
         }
     }
 }
